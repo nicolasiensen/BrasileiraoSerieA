b98e0bc7e80fa72c54bcec7d0e15b372b4ec0d8d
Nícolas Iensen <nicolas.iensen@gmail.com>
2017-09-16 17:05:58 +0200

    Throw exceptions from the api module when a request fail
    Add a config file to keep the API key, and throw exceptions from the api
    module when the response status is not 200.

--- api.js
+++ api.js
@@ -1,9 +1,20 @@
+import config from "./config.json";
+
 const API_HOST = "https://api.football-data.org/v1";
 
 export async function getLeagueTable(competitionID) {
   const response = await fetch(
-    `${API_HOST}/competitions/${competitionID}/leagueTable`
+    `${API_HOST}/competitions/${competitionID}/leagueTable`,
+    {
+      headers: new Headers({ "X-Auth-Token": config["API_KEY"] })
+    }
   );
+
   const responseJson = await response.json();
-  return responseJson;
+
+  if (response.status === 200) {
+    return responseJson;
+  } else {
+    throw responseJson.error;
+  }
 }
